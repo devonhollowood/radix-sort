7148d85cd1056bb07fae900cd1286e26287ab1b9
Devon Hollowood <devonhollowood@gmail.com>
2016-09-14 16:27:58 -0700

    Change confusing wording

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,9 @@ use std::mem;
 
 pub fn lsd_radix_sort(input: &mut [u64]) {
     let mut buckets = vec![VecDeque::<u64>::new(); 16];
-    for half_shift in 0 .. mem::size_of::<u64>()*2 {
-        let shift = half_shift * 4;
+    for nibble_shift in 0 .. mem::size_of::<u64>()*2 {
         for elem in input.iter() {
-            let bin = ((elem >> shift) & 0xF) as usize;
+            let bin = ((elem >> nibble_shift * 4) & 0xF) as usize;
             buckets[bin].push_back(elem.clone());
         }
         for (idx, elem) in buckets.iter_mut().flat_map(|v| v.drain(..)).enumerate() {
